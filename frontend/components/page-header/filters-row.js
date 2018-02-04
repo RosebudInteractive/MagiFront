5177e161d5781d4823c179c5930bf8c4f33cb5ab
Alexandr <staloverov@rosebud.ru>
2018-02-05 02:01:30 +0300

    -

--- frontend/components/page-header/filters-row.js
+++ frontend/components/page-header/filters-row.js
@@ -4,7 +4,6 @@ import {connect} from 'react-redux';
 import './desktop-filters.css';
 import * as pageHeaderActions from "../../actions/page-header-actions";
 import * as filtersActions from '../../actions/filters-actions';
-import filters from "../../reducers/filters";
 
 class FiltersRow extends React.Component {
 
@@ -35,21 +34,12 @@ class FiltersRow extends React.Component {
     }
 
     render() {
-        // let that = this;
-        // let _className = 'search-block' + (this.props.showSearchForm ? ' opened' : '');
-
         return (
             <div className="page-header__row filters-row opened">
                 <div className="page-header__wrapper filters-row__wrapper">
                     <div className="filters-row__inner">
                         <p className="filters-row__label" onClick={::this._clearFilter}>Сбросить фильтры</p>
                         <ul className="filters-list">
-                            {/*<li>*/}
-                            {/*<div className="filter-btn">*/}
-                            {/*<span className="filter-btn__title">Литература <span className="filter-btn__index">12</span></span>*/}
-                            {/*</div>*/}
-
-                            {/*</li>*/}
                             {this._getFilters()}
                         </ul>
                     </div>
