e1ffd3ba25f9946ff3565720866c601ecdac7526
SashaSokolov <alexander.f.sokolov@gmail.com>
2018-04-12 14:52:04 +0300

    Allow HTTP logger in development only.

--- server.js
+++ server.js
@@ -6,7 +6,6 @@
 const path = require('path');
 const fs = require('fs');
 const _ = require('lodash');
-const morgan = require('morgan');
 
 let webpack = require('webpack')
 let webpackDevMiddleware = require('webpack-dev-middleware');
@@ -40,7 +39,7 @@ Promise.resolve()
         let address = magisteryConfig.http.address;
 
         if (NODE_ENV === 'development') {
-            app.use(morgan('dev')); // log HTTP requests
+            app.use(require('morgan')('dev')); // log HTTP requests
             let compiler = webpack(webpackConfig);
             try {
                 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: webpackConfig.output.publicPath}));
