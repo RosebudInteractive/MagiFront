2346da635f04cb1adb37e57653cb37e88bfa489f
Alexandr <staloverov@rosebud.ru>
2018-04-16 07:26:31 +0300

    handling first tap

--- frontend/App.js
+++ frontend/App.js
@@ -43,7 +43,10 @@ class App extends Component {
         };
         this._handleScroll = this._handleScroll.bind(this);
 
-        let _isMobile = Platform.product === 'iPad';
+        let _isMobile = ((Platform.os.family === 'Andriod') || (Platform.os.family === "iOS"));
+        if (_isMobile) {
+            this.props.appActions.setAppTypeMobile()
+        }
     }
 
     get width() {
