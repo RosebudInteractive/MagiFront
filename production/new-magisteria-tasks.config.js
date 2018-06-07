8398ce57e23adc0279115c15b9703cb4914cb21d
SashaSokolov <alexander.f.sokolov@gmail.com>
2018-06-07 18:50:02 +0300

    Setting pm2 task name.

--- production/new-magisteria-tasks.config.js
+++ production/new-magisteria-tasks.config.js
@@ -6,6 +6,7 @@ module.exports = {
      * http://pm2.keymetrics.io/docs/usage/application-declaration/
      */
     apps: [{
+        name: 'Scheduled Tasks',
         script: './index.js',
         cwd: '/var/www/sites/magisteria/MagisteriaTwo/tasks',
         instance_var: 'INSTANCE_ID',
