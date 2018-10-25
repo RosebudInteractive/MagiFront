b7665b18dfd8bbb52c862164e19f87c147b2e855
levan <syroszx@gmail.com>
2018-10-25 17:46:01 +0300

    mask image

--- frontend/components/course/course-module-lazyload.js
+++ frontend/components/course/course-module-lazyload.js
@@ -49,17 +49,16 @@ class ImageBlock extends React.Component {
     render() {
         const {cover, mask, size} = this.props;
         let _width = size ? size.width : 574,
-            _height = size ? size.height : 503,
-            _deltaX = 0;
+            _height = size ? size.height : 503;
 
-        if (_height < 503) {
-            let _ratio = 503 / _height,
-                _newWidth = _width * _ratio;
-
+        // if (_height < 503) {
+        let _ratio = 503 / _height,
+            _newWidth = _width * _ratio,
             _deltaX = Math.round((_width - _newWidth) / 2);
-            _width = _newWidth;
-            _height = 503;
-        }
+
+        _width = _newWidth;
+        _height = 503;
+        // }
 
         const _image = '<image preserveAspectRatio="xMaxYMax slice" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/data/' + cover + '" x="' + _deltaX + '" width="' + _width + '" height="' + _height + '"/>';
 
