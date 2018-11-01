8df815a56f9ea0e616c5038681826862d248ebf9
SashaSokolov <alexander.f.sokolov@gmail.com>
2018-11-01 23:57:25 +0300

    "SetPosition" test.

--- const/http-codes.js
+++ const/http-codes.js
@@ -1,6 +1,6 @@
 'use strict';
 
-exports.HttpCode = {
+let HttpCode = {
     OK: 200,
     ERR_BAD_REQ: 400,
     ERR_UNAUTH: 401,
@@ -13,3 +13,20 @@ exports.HttpCode = {
     ERR_INTERNAL: 500,
     ERR_NIMPL: 501
 };
+
+let HttpMessage = {};
+
+HttpMessage[HttpCode.OK] = "OK";
+HttpMessage[HttpCode.ERR_BAD_REQ] = "Bad Request";
+HttpMessage[HttpCode.ERR_UNAUTH] = "Unauthorized";
+HttpMessage[HttpCode.ERR_PAYMENT_REQUIRED] = "Payment Required";
+HttpMessage[HttpCode.ERR_FORBIDDEN] = "Forbidden";
+HttpMessage[HttpCode.ERR_NOT_FOUND] = "Not Found";
+HttpMessage[HttpCode.ERR_CONFLICT] = "Conflict";
+HttpMessage[HttpCode.ERR_TOO_LARGE] = "Payload Too Large";
+HttpMessage[HttpCode.ERR_TOO_MANY_REQ] = "Too Many Requests";
+HttpMessage[HttpCode.ERR_INTERNAL] = "Internal Server Error";
+HttpMessage[HttpCode.ERR_NIMPL] = "Not Implemented";
+
+exports.HttpCode = HttpCode;
+exports.HttpMessage = HttpMessage;
--- test-lib/start-tests.js
+++ test-lib/start-tests.js
@@ -1,12 +1,24 @@
 'use strict';
 const { UploadFiles } = require('./upload-files');
+const { SetPosition } = require('./set-position');
 
-UploadFiles.getCourse(17, false)
+if (false)
+    UploadFiles.getCourse(17, false)
+        .then(result => {
+            if (result && (result.isErr === false))
+                console.log(result)
+            else
+                throw Error(`Invalid result: "${JSON.stringify(result)}"`);
+        })
+        .catch(err => {
+            console.error(err);
+            console.log(err);
+        });
+
+let token;
+SetPosition.setPositionTest(1000)
     .then(result => {
-        if (result && (result.isErr === false))
-            console.log(result)
-        else
-            throw Error(`Invalid result: "${JSON.stringify(result)}"`);
+        console.log(`Result: ${JSON.stringify(result)}`);
     })
     .catch(err => {
         console.error(err);
