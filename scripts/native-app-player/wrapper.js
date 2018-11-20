14d2fa1031b403e464ff035086843591e2edc2cf
levan <syroszx@gmail.com>
2018-11-20 22:52:25 +0300

    android player test

--- scripts/native-app-player/wrapper.js
+++ scripts/native-app-player/wrapper.js
@@ -175,12 +175,19 @@ export default class NativeAppPlayer {
 
     _sendMessageToApp(props) {
         props['playerId'] = this._id
-        if (this._debug || _isAndroid) {
+        if (this._debug) {
             window.postMessage(
                 JSON.stringify(props),
                 '*'
             )
             console.log(JSON.stringify(props))
+        } else if ( _isAndroid) {
+            setTimeout(() => {
+                window.postMessage(
+                    JSON.stringify(props),
+                    '*'
+                )
+            }, 100)
         } else {
             window.postMessage(
                 JSON.stringify(props)
