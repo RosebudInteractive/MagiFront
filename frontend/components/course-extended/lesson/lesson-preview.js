f477270c451ce7ec4a094be6b2d6ba04115186b8
Alexandr <staloverov@pmtech.ru>
2018-12-24 11:45:48 +0300

    release bug-fix: - gallery-button position (iPhone) - remove mock-link on draft lesson

--- frontend/components/course-extended/lesson/lesson-preview.js
+++ frontend/components/course-extended/lesson/lesson-preview.js
@@ -5,7 +5,7 @@ export default class LessonPreview extends React.Component {
         return (
             <li className="lecture-full lecture-full--archive">
                 <div className="lecture-full__wrapper">
-                    <h3 className="lecture-full__archive-title"><a href="#">{this.props.title + ' '}</a></h3>
+                    <h3 className="lecture-full__archive-title">{this.props.title + ' '}</h3>
                     <div className="lecture-full__archive-date">{this.props.readyDate}</div>
                 </div>
             </li>
