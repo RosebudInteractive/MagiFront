40253eff89b965262d23dc6fa8c1124344782c9b
levan <syroszx@gmail.com>
2019-03-13 11:13:47 +0300

    #39

--- frontend/components/combined-lesson-page/sources.js
+++ frontend/components/combined-lesson-page/sources.js
@@ -4,6 +4,7 @@ import Books from "../books";
 import LessonBooksList from "../books/lesson-books-list";
 import Aggregators from '../aggregators/aggregators'
 import LessonAggregators from './lesson-aggregators'
+import $ from "jquery"
 
 export default class Sources extends React.Component {
 
@@ -13,7 +14,8 @@ export default class Sources extends React.Component {
 
     render() {
         let {lesson} = this.props,
-            _needShowBooks = lesson.Books && Array.isArray(lesson.Books) && (lesson.Books.length > 0)
+            _needShowBooks = lesson.Books && Array.isArray(lesson.Books) && (lesson.Books.length > 0),
+            _needShowAggregators = !!lesson.ExtLinks && !$.isEmptyObject(lesson.ExtLinks)
 
         return _needShowBooks
             ?
@@ -21,7 +23,13 @@ export default class Sources extends React.Component {
                 <div className="lecture-sources">
                     <Aggregators title={'Лекция на других ресурсах'} titleClass={"other-sources__title"}
                                  extLinks={lesson.ExtLinks}/>
-                    <Books books={lesson.Books} titleClassName={"books__headline"} listClass={LessonBooksList}/>
+                    {
+                        _needShowAggregators
+                        ?
+                            <Books books={lesson.Books} titleClassName={"books__headline"} listClass={LessonBooksList}/>
+                            :
+                            <Books books={lesson.Books} titleClassName={"books__headline lecture-info__extras-label"} listClass={LessonBooksList} extClass={"lecture-info__extras"}/>
+                    }
                 </div>
             </div>
             :
