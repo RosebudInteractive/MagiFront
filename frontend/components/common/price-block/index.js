89025cbe5d2348482a17c967b5d33b2a0af817b0
levan <syroszx@gmail.com>
2019-03-28 12:37:55 +0300

    #45

--- frontend/components/common/price-block/index.js
+++ frontend/components/common/price-block/index.js
@@ -16,18 +16,35 @@ export default class PriceBlock extends React.Component {
             return null
         }
 
+        let _hasDiscount = course.DPrice && course.Discount && course.Discount.Perc,
+            _hasDiscountDescr = _hasDiscount && course.Discount.Description;
+
         return <div className="course-module__price-block">
             <div className="course-module__price-block-wrapper">
                 <a href="#" className="btn btn--brown course-module__price-btn">Купить</a>
                 <div className="course-module__price-block-section">
-                    <p className="course-module__price">550₽ <span className="discount">-80%</span></p>
-                    <p className="course-module__old-price">5580₽</p>
+                    {
+                        _hasDiscount ?
+                            <React.Fragment>
+                                <p className="course-module__price">{course.DPrice + "₽ "}<span className="discount">{`-${course.Discount.Perc}%`}</span></p>
+                                <p className="course-module__old-price">{course.Price + "₽"}</p>
+                            </React.Fragment>
+                            :
+                            <p className="course-module__price">{course.Price + "₽"}</p>
+                    }
                 </div>
             </div>
-            <div className="course-module__price-block-wrapper">
-                <p className="course-module__price-block-info"><span className="label">Персональная скидка.</span> Мы
-                    думаем что вам понравится потому что вы посмотрели курс «Иоанн Креститель».</p>
-            </div>
+            {
+                _hasDiscountDescr ?
+                    <div className="course-module__price-block-wrapper">
+                        <p className="course-module__price-block-info">
+                            <span className="label">Персональная скидка.</span>
+                            {" " + course.Discount.Description}
+                        </p>
+                    </div>
+                    :
+                    null
+            }
         </div>
     }
 }
