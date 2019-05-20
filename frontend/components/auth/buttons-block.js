ab106a85714bf274f2467da5db385f66a25aaf9d
levan <syroszx@gmail.com>
2019-05-20 09:00:54 +0300

    #56

--- frontend/components/auth/buttons-block.js
+++ frontend/components/auth/buttons-block.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
 import {AUTHORIZATION_STATE} from '../../constants/user'
 
 import * as userActions from '../../actions/user-actions'
 
 class ButtonsBlock extends React.Component {
-
-    _responseFacebook(response) {
-        console.log(response);
-    }
-
 
     render() {
         const _google = '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#google"/>',
@@ -26,27 +20,27 @@ class ButtonsBlock extends React.Component {
                     <p className="register-block__title">Регистрация с помощью</p>
             }
 
-            <Link to={'/api/googlelogin'} target={"_blank"}
+            <a href={'/api/googlelogin?redirect=/category/pushkin'}
                   className="btn btn--white register-block__btn">
                         <span className="icon">
                             <svg width="16" height="16" dangerouslySetInnerHTML={{__html: _google}}/>
                         </span>
                 <span className="text">Google</span>
-            </Link>
-            <Link to={'/api/vklogin'} target={"_blank"}
+            </a>
+            <a href={'/api/vklogin?redirect=/category/pushkin'}
                   className="btn btn--white register-block__btn">
                         <span className="icon">
                             <svg width="18" height="11" dangerouslySetInnerHTML={{__html: _vk}}/>
                         </span>
                 <span className="text">Вконтакте</span>
-            </Link>
-            <Link to={'/api/fblogin'} target={"_blank"}
+            </a>
+            <a href={'/api/fblogin?redirect=/category/pushkin'}
                   className="btn btn--white register-block__btn register-block__btn--fullwidth">
                         <span className="icon">
                             <svg width="16" height="16" dangerouslySetInnerHTML={{__html: _facebook}}/>
                         </span>
                 <span className="text">Facebook</span>
-            </Link>
+            </a>
         </div>
     }
 }
