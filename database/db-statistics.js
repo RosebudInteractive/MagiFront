98921ab8ac51576ffb831f393f633ec9979d329d
SashaSokolov <alexander.f.sokolov@gmail.com>
2019-06-12 14:07:20 +0300

    Course sales statistics.

--- database/db-statistics.js
+++ database/db-statistics.js
@@ -23,6 +23,99 @@ const GET_STAT_MSSQL =
 const GET_STAT_MYSQL =
     "call <%= stat_func %>('<%= first_date %>', '<%= last_date %>')";
 
+const GET_USER_PURCHASE_MSSQL =
+    "select c.[ChequeDate], u.[RegDate], c.[UserId], u.[DisplayName],\n" +
+    "  u.[Email], ii.[Name] Course, p.[Price] PriceIni, (p.[Price] - ii.[Price]) Discount, ii.[Price],\n" +
+    "  coalesce(g.[Campaign] + ' (' + g.[Source] + '+' + g.[Medium] + ')', '') Campaign, coalesce(pc.[Code], '') as Promo,\n" +
+    "  round(coalesce(sum(h.[LsnTime]), 0) / 3600, 2) as LsnTime, round(sum(lg.[Duration]) / 3600, 2) as CourseDuration,\n" +
+    "  round(coalesce(sum(h.[LsnTime]), 0) / sum(lg.[Duration]), 2) as LsnPart\n" +
+    "from[Cheque] c\n" +
+    "  join[Invoice] i on i.[Id] = c.[InvoiceId]\n" +
+    "  join[InvoiceItem] ii on ii.[InvoiceId] = i.[Id]\n" +
+    "  left join[Price] p on p.[ProductId] = ii.[ProductId] and(p.[FirstDate] <= c.[ChequeDate])\n" +
+    "  and((p.[LastDate] is null) or(p.[LastDate] > c.[ChequeDate]))\n" +
+    "  join[User] u on u.[SysParentId] = c.[UserId]\n" +
+    "  left join[Campaign] g on g.[Id] = c.[CampaignId]\n" +
+    "  left join[PromoCode] pc on pc.[Id] = c.[PromoCodeId]\n" +
+    "  join[Course] cs on cs.[ProductId] = ii.[ProductId]\n" +
+    "  join[LessonCourse] lc on lc.[CourseId] = cs.[Id]\n" +
+    "  join[LessonLng] lg on lg.[LessonId] = lc.[LessonId]\n" +
+    "  left join[LsnHistory] h on h.[UserId] = c.[UserId] and h.[LessonId] = lc.[LessonId]\n" +
+    "where(c.[ChequeTypeId] = 1) and(c.[StateId] = 4) and(c.[ChequeDate] >= convert(datetime, '<%= first_date %>'))\n" +
+    "  and(c.[ChequeDate] < convert(datetime, '<%= last_date %>'))\n" +
+    "group by c.[ChequeDate], u.[RegDate], c.[UserId], u.[DisplayName], u.[Email], ii.[Name],\n" +
+    "  p.[Price], ii.[Price], g.[Campaign], g.[Source], g.[Medium], pc.[Code]\n" +
+    "order by c.[ChequeDate] desc";
+
+const GET_USER_PURCHASE_MYSQL =
+    "select c.`ChequeDate`, u.`RegDate`, c.`UserId`, u.`DisplayName`,\n" +
+    "  u.`Email`, ii.`Name` Course, p.`Price` PriceIni, (p.`Price` - ii.`Price`) Discount, ii.`Price`,\n" +
+    "  coalesce(concat(g.`Campaign`, ' (', g.`Source`, '+', g.`Medium`, ')'), '') Campaign, coalesce(pc.`Code`, '') as Promo,\n" +
+    "  round(coalesce(sum(h.`LsnTime`), 0) / 3600, 2) as LsnTime, round(sum(lg.`Duration`) / 3600, 2) as CourseDuration,\n" +
+    "  round(coalesce(sum(h.`LsnTime`), 0) / sum(lg.`Duration`), 2) as LsnPart\n" +
+    "from`Cheque` c\n" +
+    "  join`Invoice` i on i.`Id` = c.`InvoiceId`\n" +
+    "  join`InvoiceItem` ii on ii.`InvoiceId` = i.`Id`\n" +
+    "  left join`Price` p on p.`ProductId` = ii.`ProductId` and(p.`FirstDate` <= c.`ChequeDate`)\n" +
+    "  and((p.`LastDate` is null) or(p.`LastDate` > c.`ChequeDate`))\n" +
+    "  join`User` u on u.`SysParentId` = c.`UserId`\n" +
+    "  left join`Campaign` g on g.`Id` = c.`CampaignId`\n" +
+    "  left join`PromoCode` pc on pc.`Id` = c.`PromoCodeId`\n" +
+    "  join`Course` cs on cs.`ProductId` = ii.`ProductId`\n" +
+    "  join`LessonCourse` lc on lc.`CourseId` = cs.`Id`\n" +
+    "  join`LessonLng` lg on lg.`LessonId` = lc.`LessonId`\n" +
+    "  left join`LsnHistory` h on h.`UserId` = c.`UserId` and h.`LessonId` = lc.`LessonId`\n" +
+    "where(c.`ChequeTypeId` = 1) and(c.`StateId` = 4) and(c.`ChequeDate` >= '<%= first_date %>') and(c.`ChequeDate` < '<%= last_date %>')\n" +
+    "group by c.`ChequeDate`, u.`RegDate`, c.`UserId`, u.`DisplayName`, u.`Email`, ii.`Name`,\n" +
+    "  p.`Price`, ii.`Price`, g.`Campaign`, g.`Source`, g.`Medium`, pc.`Code`\n" +
+    "order by c.`ChequeDate` desc";
+
+const GET_COURSE_PURCHASE_MSSQL =
+    "select t.Course, sum(t.Qty) Qty, sum(t.TotSum) TotSum, sum(t.GiftQty) GiftQty\n" +
+    "from\n" +
+    "  (select cl.[Name] Course, count(*) Qty, sum(ii.[Price] * ii.[Qty]) TotSum, 0 as GiftQty\n" +
+    "  from[Cheque] c\n" +
+    "    join[Invoice] i on i.[Id] = c.[InvoiceId]\n" +
+    "    join[InvoiceItem] ii on ii.[InvoiceId] = i.[Id]\n" +
+    "    join[Course] cs on cs.[ProductId] = ii.[ProductId]\n" +
+    "    join[CourseLng] cl on cl.[CourseId] = cs.[Id]\n" +
+    "    left join[UserGiftCourse] gc on gc.[UserId] = c.[UserId] and gc.[CourseId] = cs.[Id]\n" +
+    "  where(c.[ChequeTypeId] = 1) and(c.[StateId] = 4) and(c.[ChequeDate] >= convert(datetime, '<%= first_date %>'))\n" +
+    "    and(c.[ChequeDate] < convert(datetime, '<%= last_date %>'))\n" +
+    "  group by cl.[Name]\n" +
+    "  union all\n" +
+    "  select cl.[Name] Course, 0, 0, count(*)\n" +
+    "  from[UserGiftCourse] gc\n" +
+    "    join[CourseLng] cl on cl.[CourseId] = gc.[CourseId]\n" +
+    "  where(gc.[TimeCr] >= convert(datetime, '<%= first_date %>'))\n" +
+    "    and(gc.[TimeCr] < convert(datetime, '<%= last_date %>'))\n" +
+    "  group by cl.[Name]) as t\n" +
+    "group by t.Course\n" +
+    "order by 3 desc";
+
+const GET_COURSE_PURCHASE_MYSQL =
+    "select t.Course, sum(t.Qty) Qty, sum(t.TotSum) TotSum, sum(t.GiftQty) GiftQty\n" +
+    "from\n" +
+    "  (select cl.`Name` Course, count(*) Qty, sum(ii.`Price` * ii.`Qty`) TotSum, 0 as GiftQty\n" +
+    "  from`Cheque` c\n" +
+    "    join`Invoice` i on i.`Id` = c.`InvoiceId`\n" +
+    "    join`InvoiceItem` ii on ii.`InvoiceId` = i.`Id`\n" +
+    "    join`Course` cs on cs.`ProductId` = ii.`ProductId`\n" +
+    "    join`CourseLng` cl on cl.`CourseId` = cs.`Id`\n" +
+    "    left join`UserGiftCourse` gc on gc.`UserId` = c.`UserId` and gc.`CourseId` = cs.`Id`\n" +
+    "  where(c.`ChequeTypeId` = 1) and(c.`StateId` = 4) and(c.`ChequeDate` >= '<%= first_date %>')\n" +
+    "    and(c.`ChequeDate` < '<%= last_date %>')\n" +
+    "  group by cl.`Name`\n" +
+    "  union all\n" +
+    "  select cl.`Name` Course, 0, 0, count(*)\n" +
+    "  from`UserGiftCourse` gc\n" +
+    "    join`CourseLng` cl on cl.`CourseId` = gc.`CourseId`\n" +
+    "  where(gc.`TimeCr` >= '<%= first_date %>')\n" +
+    "    and(gc.`TimeCr` < '<%= last_date %>')\n" +
+    "  group by cl.`Name`) as t\n" +
+    "group by t.Course\n" +
+    "order by 3 desc";
+
 const DEFAULT_FIRST_DATE = new Date("2019-05-08T00:00:00+0300");
 
 const DbStatistics = class DbStatistics extends DbObject {
@@ -55,6 +148,40 @@ const DbStatistics = class DbStatistics extends DbObject {
         return this._stat_report(caption, "stat_report_by_campaign", opts);
     }
 
+    async user_course_purchase(options) {
+        let opts = _.cloneDeep(options);
+        opts.exclude = {
+            st_date: true,
+            fin_date: true
+        }
+        let { firsDate, lastDate } = this._getInterval(opts);
+        let caption = `Покупки курсов пользователями с ` +
+            `${this._dateToString(firsDate, true, false)} по ${this._dateToString(lastDate, true, false)}.`;
+        opts.first_date = firsDate;
+        opts.last_date = lastDate;
+        return this._stat_report(caption, {
+            mysql: GET_USER_PURCHASE_MYSQL,
+            mssql: GET_USER_PURCHASE_MSSQL
+        }, opts);
+    }
+
+    async course_sale(options) {
+        let opts = _.cloneDeep(options);
+        opts.exclude = {
+            st_date: true,
+            fin_date: true
+        }
+        let { firsDate, lastDate } = this._getInterval(opts);
+        let caption = `Продажи курсов с ` +
+            `${this._dateToString(firsDate, true, false)} по ${this._dateToString(lastDate, true, false)}.`;
+        opts.first_date = firsDate;
+        opts.last_date = lastDate;
+        return this._stat_report(caption, {
+            mysql: GET_COURSE_PURCHASE_MYSQL,
+            mssql: GET_COURSE_PURCHASE_MSSQL
+        }, opts);
+    }
+
     _getInterval(options) {
         let opts = options || {};
         let firsDate = opts.first_date ? new Date(opts.first_date) : DEFAULT_FIRST_DATE;
@@ -62,28 +189,46 @@ const DbStatistics = class DbStatistics extends DbObject {
         return { firsDate: firsDate, lastDate: lastDate };
     }
 
-    async _stat_report(caption, report_nane, options) {
+    async _stat_report(caption, report_name, options) {
         let opts = options || {};
         let dbOpts = opts.dbOptions || {};
         let data = [];
         let header = [];
 
         let { firsDate, lastDate } = this._getInterval(opts);
-
-        let result = await $data.execSql({
-            dialect: {
-                mysql: _.template(GET_STAT_MYSQL)({
-                    stat_func: report_nane,
-                    first_date: this._dateToString(firsDate, true, false),
-                    last_date: this._dateToString(lastDate, true, false)
-                }),
-                mssql: _.template(GET_STAT_MSSQL)({
-                    stat_func: report_nane,
-                    first_date: this._dateToString(firsDate, true, false),
-                    last_date: this._dateToString(lastDate, true, false)
-                })
-            }
-        }, dbOpts);
+        let dialect;
+        switch (typeof (report_name)) {
+            case "string":
+                dialect = {
+                    mysql: _.template(GET_STAT_MYSQL)({
+                        stat_func: report_name,
+                        first_date: this._dateToString(firsDate, true, false),
+                        last_date: this._dateToString(lastDate, true, false)
+                    }),
+                    mssql: _.template(GET_STAT_MSSQL)({
+                        stat_func: report_name,
+                        first_date: this._dateToString(firsDate, true, false),
+                        last_date: this._dateToString(lastDate, true, false)
+                    })
+                };
+                break;
+            default:
+                if (report_name && report_name.mysql && report_name.mssql) {
+                    dialect = {
+                        mysql: _.template(report_name.mysql)({
+                            first_date: this._dateToString(firsDate, true, false),
+                            last_date: this._dateToString(lastDate, true, false)
+                        }),
+                        mssql: _.template(report_name.mssql)({
+                            first_date: this._dateToString(firsDate, true, false),
+                            last_date: this._dateToString(lastDate, true, false)
+                        })
+                    };
+                }
+                else
+                    throw new Error(`Invalid parameter "report_name": ${report_name}`);
+        }
+        let result = await $data.execSql({ dialect: dialect }, dbOpts);
         if (result && result.detail && (result.detail.length > 0)) {
             let isFirst = true;
             result.detail.forEach(elem => {
--- services/statistics.js
+++ services/statistics.js
@@ -28,6 +28,28 @@ function setupStatistics(app) {
                 next(err);
             });
     });
+
+    app.get('/api/adm/statistics/user_course_purchase', (req, res, next) => {
+        StatisticsService()
+            .user_course_purchase(req.query)
+            .then(rows => {
+                res.send(rows);
+            })
+            .catch(err => {
+                next(err);
+            });
+    });
+
+    app.get('/api/adm/statistics/course_sale', (req, res, next) => {
+        StatisticsService()
+            .course_sale(req.query)
+            .then(rows => {
+                res.send(rows);
+            })
+            .catch(err => {
+                next(err);
+            });
+    });
 }
 
 exports.setupStatistics = setupStatistics;
