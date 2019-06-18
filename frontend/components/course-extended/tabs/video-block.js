ba2c4e09dd6b2c48186fdf605b1fec04b766cb5a
levan <syroszx@gmail.com>
2019-06-18 19:04:07 +0300

    #71

--- frontend/components/course-extended/tabs/video-block.js
+++ frontend/components/course-extended/tabs/video-block.js
@@ -1,35 +1,64 @@
 import React from 'react'
 import PropTypes from "prop-types"
 
+const RATIO = 1.6
+
 export default class VideoBlock extends React.Component {
 
     static propTypes = {
         course: PropTypes.object,
     }
 
+    constructor(props) {
+        super(props)
+
+        this._resizeHandler = () => {
+            const _intro = $('#intro-video'),
+                _interview = $('#interview-video')
+
+            if (_intro) {
+                _intro.height(_intro.width() / RATIO)
+            }
+
+            if (_interview) {
+                _interview.height(_interview.width() / RATIO)
+            }
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this._resizeHandler);
+
+        this._resizeHandler()
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this._resizeHandler);
+    }
+
     render() {
         const _visible = this.props.course && (this.props.course.VideoIntroLink || this.props.course.VideoIntwLink),
             _bothVideoExists = this.props.course.VideoIntroLink && this.props.course.VideoIntwLink,
             _introDuration = this.props.course && this.props.course.IntroDFmt && <span className="duration">{this.props.course.IntroDFmt}</span>,
-            // _introDuration = <span className="duration">7:46</span>,
             _interviewDuration = this.props.course && this.props.course.IntwDFmt && <span className="duration">{this.props.course.IntwDFmt}</span>
-            // _interviewDuration = <span className="duration">7:46</span>
 
         return _visible ?
             <div className={"course__video-block" + (!_bothVideoExists ? " _single" : "")}>
                 {
                     this.props.course.VideoIntroLink ?
                         <div className="video-block__item">
-                            <div className="video-frame__wrapper">
+                            <div className="video-frame__wrapper" id="intro-video">
                                 <iframe width="100%" height="100%" src={this.props.course.VideoIntroLink}
                                         frameBorder="0"
                                         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                                         allowFullScreen/>
                             </div>
                             <div className="video-frame__title">
-                                <span className="title">Презентация курса</span>
-                                {_introDuration}
-                                <span className="arrow">→</span>
+                                <div className="title__wrapper">
+                                    <span className="title">Презентация курса</span>
+                                    {_introDuration}
+                                    <span className="arrow">→</span>
+                                </div>
                             </div>
                         </div>
                         :
@@ -38,15 +67,18 @@ export default class VideoBlock extends React.Component {
                 {
                     this.props.course.VideoIntwLink ?
                         <div className="video-block__item">
-                            <div className="video-frame__wrapper">
-                                <iframe width="100%" height="100%" src={this.props.course.VideoIntwLink} frameBorder="0"
+                            <div className="video-frame__wrapper" id="interview-video">
+                                <iframe width="100%" height="100%" src={this.props.course.VideoIntwLink}
+                                        frameBorder="0"
                                         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                                         allowFullScreen/>
                             </div>
                             <div className="video-frame__title">
-                                <span className="title">Интервью с автором</span>
-                                {_interviewDuration}
-                                <span className="arrow">→</span>
+                                <div className="title__wrapper">
+                                    <span className="title">Интервью с автором</span>
+                                    {_interviewDuration}
+                                    <span className="arrow">→</span>
+                                </div>
                             </div>
                         </div>
                         :
