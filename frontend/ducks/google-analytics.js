6d11bc82a9ee3b216b11d2a470a5a3a8ec3e84f6
Alexandr <staloverov@pmtech.ru>
2019-06-25 17:11:34 +0300

    #77

---
+++ frontend/ducks/google-analytics.js
@@ -0,0 +1,9 @@
+import {appName} from '../config'
+
+/**
+ * Constants
+ * */
+export const moduleName = 'google-analytics'
+const prefix = `${appName}/${moduleName}`
+
+const COURSES_PAGE_SHOWED = 'COURSES_PAGE_SHOWED';
