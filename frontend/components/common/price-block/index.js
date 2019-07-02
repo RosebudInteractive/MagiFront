929a85cbf5a0cdb675d81c0737d772030096be70
levan <syroszx@gmail.com>
2019-07-02 21:12:29 +0300

    #77

--- frontend/components/common/price-block/index.js
+++ frontend/components/common/price-block/index.js
@@ -87,24 +87,22 @@ class PriceBlock extends React.Component {
         let _author = 'unknown',
             _category = 'unknown';
 
-        if (course.hasOwnProperty("AuthorsObj") && course.hasOwnProperty("CategoriesObj")) {
+        if (course.hasOwnProperty("AuthorsObj")) {
             _author = course.AuthorsObj && course.AuthorsObj[0] ? course.AuthorsObj[0].FirstName + " " + course.AuthorsObj[0].LastName : ''
-            _category = course.CategoriesObj && course.CategoriesObj[0] ? course.CategoriesObj[0].Name : '';
-        }
-
-        if (course.hasOwnProperty("Authors") && course.hasOwnProperty("Categories")) {
-            _author = course.Authors && course.Authors[0] ? course.Authors[0].FirstName + " " + course.Authors[0].LastName : ''
-            _category = course.Categories && course.Categories[0] ? course.Categories[0].Name : ''
-        }
-
-        if (course.hasOwnProperty("authors") && course.hasOwnProperty("categories")) {
-            _author = course.authors && course.authors[0] ? course.authors[0].FirstName + " " + course.authors[0].LastName : ''
+        } else if (course.hasOwnProperty("authors")) {
             _category = course.categories && course.categories[0] ? course.categories[0].Name : ''
+        } else if (course.hasOwnProperty("Authors")) {
+            _author = course.Authors && course.Authors[0] ? course.Authors[0].FirstName + " " + course.Authors[0].LastName : ''
+        }  else if (course.hasOwnProperty("author")) {
+            _author = course.author
         }
 
-        if (course.hasOwnProperty("author")) {
-            _author = course.author
-            // _category = course.Categories && course.Categories[0] ? course.Categories[0].Name : ''
+        if (course.hasOwnProperty("CategoriesObj")) {
+            _category = course.CategoriesObj && course.CategoriesObj[0] ? course.CategoriesObj[0].Name : '';
+        } else if (course.hasOwnProperty("categories")) {
+            _category = course.categories && course.categories[0] ? course.categories[0].Name : ''
+        } else if (course.hasOwnProperty("Categories")) {
+            _category = course.Categories && course.Categories[0] ? course.Categories[0].Name : ''
         }
 
         return {author: _author, category: _category}
