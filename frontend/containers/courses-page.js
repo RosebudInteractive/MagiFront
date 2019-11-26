6e5865f5362d0bd564d8b6286917f720c2b1e52b
levan <syroszx@gmail.com>
2019-11-27 00:45:53 +0300

    #144

--- frontend/containers/courses-page.js
+++ frontend/containers/courses-page.js
@@ -141,7 +141,7 @@ class CoursesPage extends React.Component {
                 }
             })
             .forEach((course, index, array) => {
-                let _needLazyLoading = array.length < _courses.length
+                let _needLazyLoading = (array.length > 3)
 
                 if (course.Id === fixedCourseId) {
                     _result.unshift(<FixCourseWrapper course={course}/>)
