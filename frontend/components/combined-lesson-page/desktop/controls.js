5e506ced1ead7436c98c22471b3253cc34458046
Alexandr <syroszx@gmail.com>
2020-04-23 09:01:35 +0300

    #192

--- frontend/components/combined-lesson-page/desktop/controls.js
+++ frontend/components/combined-lesson-page/desktop/controls.js
@@ -5,6 +5,20 @@ import $ from 'jquery'
 
 import * as playerStartActions from '../../../actions/player-start-actions'
 import {isMobileAppleDevice} from "tools/page-tools";
+import {Link} from "react-router-dom";
+import LessonTooltip from "../lesson-tooltip";
+import {lessonsSelector} from "ducks/lesson-menu";
+
+const SVG = {
+    BACKWARD: '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#backward"/>',
+    FORWARD: '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#forward"/>',
+    PAUSE: '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#pause"/>',
+    PLAY: '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"/>',
+    SOUND: '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#sound"/>',
+    MUTE: '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#mute"/>',
+    PREV: '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#player-prev"/>',
+    NEXT: '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#player-next"/>',
+}
 
 class Controls extends React.Component {
 
@@ -18,55 +32,68 @@ class Controls extends React.Component {
         });
     }
 
-    _onBackward() {
-        let _newPosition = (this.props.currentTime < 10) ? 0 : (this.props.currentTime - 10);
-        this.props.playerStartActions.startSetCurrentTime(_newPosition);
-    }
-
-    _setVolume(current, total) {
-        let value = total ? (current / total) : 0
-        this.props.playerStartActions.startSetVolume(value)
-    }
-
-    _startPlay() {
-        this.props.playerStartActions.preinitAudios(this.props.audios);
-        this.props.playerStartActions.startPlay(this.props.lesson.Id)
-    }
-
     render() {
-        const _backwards = '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#backward"/>',
-            _pause = '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#pause"/>',
-            _play = '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"/>',
-            _sound = '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#sound"/>',
-            _mute = '<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#mute"/>';
-
-        let _id = this.props.lesson ? this.props.lesson.Id : '',
+        let {lesson, course, isPaidCourse} = this.props,
+            _id = lesson ? lesson.Id : '',
             _isIOS = isMobileAppleDevice(),
             _needHideSoundControl = (this.props.muted || _isIOS)
+
+        let {prev, next} = this._getSiblingsLessons()
 
         return (
             <div className="player-block__controls desktop">
                 <button type="button" className="backwards" onClick={::this._onBackward}>
-                    <svg width="18" height="11" dangerouslySetInnerHTML={{__html: _backwards}}/>
+                    <svg width="18" height="11" dangerouslySetInnerHTML={{__html: SVG.BACKWARD}}/>
                 </button>
                 {this.props.paused ?
                     <button type="button" className="play-button" onClick={::this._startPlay}>
-                        <svg className="play" width="19" height="19" dangerouslySetInnerHTML={{__html: _play}}/>
+                        <svg className="play" width="19" height="19" dangerouslySetInnerHTML={{__html: SVG.PLAY}}/>
                     </button>
                     :
                     <button type="button" className="play-button paused"
                             onClick={::this.props.playerStartActions.startPause}>
-                        <svg className="pause" width="11" height="18" dangerouslySetInnerHTML={{__html: _pause}}/>
+                        <svg className="pause" width="11" height="18" dangerouslySetInnerHTML={{__html: SVG.PAUSE}}/>
                     </button>
                 }
+                <button type="button" className="forward" onClick={::this._onForward}>
+                    <svg width="22" height="16" dangerouslySetInnerHTML={{__html: SVG.FORWARD}}/>
+                </button>
+                <div className="lesson-navigate-block">
+                    {
+                        prev ?
+                            <Link to={`/${course.URL}/${prev.URL}`} className="prev-lesson">
+                                <button type="button" className="forward">
+                                    <svg width="32" height="26" dangerouslySetInnerHTML={{__html: SVG.PREV}}/>
+                                </button>
+                                <LessonTooltip currentLessonUrl={lesson.URL} isPaidCourse={isPaidCourse} course={course} lesson={prev} title={"Предыдущий эпизод"}/>
+                            </Link>
+                            :
+                            <button type="button" className="forward _disabled">
+                                <svg width="32" height="26" dangerouslySetInnerHTML={{__html: SVG.PREV}}/>
+                            </button>
+                    }
+                    {
+                        next ?
+                            <Link to={`/${course.URL}/${next.URL}`} className="next-lesson">
+                                <button type="button" className="forward">
+                                    <svg width="32" height="26" dangerouslySetInnerHTML={{__html: SVG.NEXT}}/>
+                                </button>
+                                <LessonTooltip currentLessonUrl={lesson.URL} isPaidCourse={isPaidCourse} course={course} lesson={next}/>
+                            </Link>
+                            :
+                            <button type="button" className="forward _disabled">
+                                <svg width="32" height="26" dangerouslySetInnerHTML={{__html: SVG.NEXT}}/>
+                            </button>
+                    }
+                </div>
                 <button type="button" className="sound-button"
                         style={_isIOS ? {display: 'none'} : null}
                         onClick={::this.props.playerStartActions.toggleMute}>
                     {
                         this.props.muted ?
-                            <svg className="off" width="18" height="18" dangerouslySetInnerHTML={{__html: _mute}}/>
+                            <svg className="off" width="18" height="18" dangerouslySetInnerHTML={{__html: SVG.MUTE}}/>
                             :
-                            <svg className="on" width="18" height="18" dangerouslySetInnerHTML={{__html: _sound}}/>
+                            <svg className="on" width="18" height="18" dangerouslySetInnerHTML={{__html: SVG.SOUND}}/>
                     }
                 </button>
                 <div className="sound-control" id={'sound-bar' + _id}
@@ -81,6 +108,47 @@ class Controls extends React.Component {
             </div>
         );
     }
+
+    _onBackward() {
+        let _newPosition = (this.props.currentTime < 10) ? 0 : (this.props.currentTime - 10);
+        this.props.playerStartActions.startSetCurrentTime(_newPosition);
+    }
+
+    _onForward() {
+        let _newPosition = (this.props.totalDuration - this.props.currentTime < 10) ? 0 : (this.props.currentTime + 10);
+        this.props.playerStartActions.startSetCurrentTime(_newPosition);
+    }
+
+    _setVolume(current, total) {
+        let value = total ? (current / total) : 0
+        this.props.playerStartActions.startSetVolume(value)
+    }
+
+    _startPlay() {
+        this.props.playerStartActions.preinitAudios(this.props.audios);
+        this.props.playerStartActions.startPlay(this.props.lesson.Id)
+    }
+
+    _getSiblingsLessons() {
+        const {lessonList} = this.props
+
+        let _lessons = []
+        lessonList.forEach((lesson) => {
+            _lessons.push(lesson)
+            if (lesson.Lessons && (lesson.Lessons.length > 0)) {
+                lesson.Lessons.forEach((sublesson) => {_lessons.push(sublesson)})
+            }
+        })
+
+        let _index = _lessons.findIndex((lesson) => {
+            return lesson.Id === this.props.lesson.Id
+        })
+
+        return {
+            prev: (_index > 0) ? _lessons[_index - 1] : null,
+            next: (_index < _lessons.length - 2) ? _lessons[_index + 1] : null
+        }
+    }
 }
 
 function mapStateToProps(state) {
@@ -89,6 +157,7 @@ function mapStateToProps(state) {
         paused: state.player.paused,
         muted: state.player.muted,
         volume: state.player.volume,
+        lessonList: lessonsSelector(state),
     }
 }
 
--- frontend/components/combined-lesson-page/lesson-tooltip/index.js
+++ frontend/components/combined-lesson-page/lesson-tooltip/index.js
@@ -11,15 +11,20 @@ export default class LessonTooltip extends React.Component {
         lesson: PropTypes.object,
         course: PropTypes.object,
         isPaidCourse: PropTypes.bool,
+        title: PropTypes.string,
     }
 
+    static defaultProps = {
+        title: "Следующий эпизод"
+    };
+
     render() {
-        const {lesson, course, isPaidCourse, currentLessonUrl} = this.props
+        const {lesson, course, isPaidCourse, currentLessonUrl, title} = this.props
 
         return <div className="lesson-tooltip">
             <PlayBlock lesson={lesson} course={course} isPaidCourse={isPaidCourse} courseUrl={course.URL} lessonUrl={currentLessonUrl}/>
             <div className="lesson-tooltip__caption">
-                <div className="caption__title font-universal__title-smallx">Следующий эпизод</div>
+                <div className="caption__title font-universal__title-smallx">{title}</div>
                 <Link to={`/${course.URL}/${lesson.URL}`} className="caption__lesson-name font-universal__book-medium">
                     <span className="number">{`${lesson.Number}. `}</span>
                     <span className="text">{lesson.Name}</span>
