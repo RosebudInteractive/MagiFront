4089a6bb0387e59fb7d22c26efb8c06aa0fb17a8
Alexandr <syroszx@gmail.com>
2020-04-27 10:51:11 +0300

    #192

--- frontend/components/test-page/result/answers/comment-block/index.js
+++ frontend/components/test-page/result/answers/comment-block/index.js
@@ -82,6 +82,6 @@ export default class CommentBlock extends React.Component {
     }
 
     _getMaxHeight() {
-        return $(window).width() > 414 ? 68 : 153
+        return $(window).width() > 414 ? 68 : 160
     }
 }
