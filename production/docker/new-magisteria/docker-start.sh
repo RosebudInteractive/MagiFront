da362a871bbaef88208a5be2e3198d6cb4efde19
SashaSokolov <alexander.f.sokolov@gmail.com>
2020-10-13 10:49:30 +0300

    Docker configs.

--- production/docker/new-magisteria/docker-start.sh
+++ production/docker/new-magisteria/docker-start.sh
@@ -1,15 +1,18 @@
-BuildVar = "${BUILD:-'SKIP'}"
-ModeVar = "${MODE:-'PM2'}"
-cd app/src/node-v12/MagisteriaTwo
+BuildVar = ${BUILD:-'SKIP'}
+ModeVar = ${MODE:-'PM2'}
+cd /app/src/node-v12/MagisteriaTwo
 if [ BuildVar == 'ALL' ] || [ BuildVar == 'MAG' ]
 then
   npm install
-cd app/src/node-v12/Uccello2
+fi
+cd /app/src/node-v12/Uccello2
 if [ BuildVar == 'ALL' ] || [ BuildVar == 'LIB' ]
 then
   npm install
-cd app/src/node-v12
+fi
+cd /app/src/node-v12
 
 if [ ModeVar == 'PM2' ]
 then
   pm2-runtime start ./MagisteriaTwo/production/new-magisteria-docker.config.js --error "/app/logs/" --output "/app/logs/"
+fi
