6186820cdebebcc40bcbcd9cb409bb65b10c36c2
SashaSokolov <alexander.f.sokolov@gmail.com>
2021-06-04 13:34:42 +0300

    Magisteria Team Task #3.1 (build 38).

--- services/pm/index.js
+++ services/pm/index.js
@@ -1,16 +1,67 @@
 const _ = require('lodash');
 const config = require('config');
+const { NotificationParams } = require('./const');
 const { ProcessService } = require('./process-api');
+const { NotificationService } = require('./notification');
+const { getTimeStr, buildLogString } = require('../../utils');
 
 const ROUTE_PREFIX = "/api/pm/";
 
+async function markNotifAsRead(req) {
+    if (req.user && req.query && req.query[NotificationParams.ParamName]) {
+        try {
+            await NotificationService().markAsRead([req.query[NotificationParams.ParamName]], { user: req.user });
+        }
+        catch (err) {
+            console.error(buildLogString(`NotificationService().markAsRead: "${err && err.message ? err.message : err}"`));
+        }
+    }
+}
+
 function setupProcesses(app) {
+
+    if (!global.$Services)
+        global.$Services = {};
+    global.$Services.notifications = NotificationService;
 
     if (!global.$Services)
         global.$Services = {};
     global.$Services.processes = ProcessService;
 
     if (app) {
+
+        app.get(`${ROUTE_PREFIX}notification-list`, async (req, res, next) => {
+            try {
+                let opts = _.defaultsDeep({ user: req.user }, req.query);
+                let rows = await NotificationService().getList(opts);
+                res.send(rows);
+            }
+            catch (err) {
+                next(err);
+            }
+        });
+
+        app.put(`${ROUTE_PREFIX}notification/mark-as-read`, async (req, res, next) => {
+            try {
+                let opts = _.defaultsDeep({ user: req.user, is_from_request: true }, req.query);
+                let rows = await NotificationService().markAsRead(req.body, opts);
+                res.send(rows);
+            }
+            catch (err) {
+                next(err);
+            }
+        });
+
+        app.post(`${ROUTE_PREFIX}notification`, async (req, res, next) => {
+            try {
+                let opts = _.defaultsDeep({ user: req.user, is_from_request: true }, req.query);
+                let rows = await NotificationService().newNotification(req.body, opts);
+                res.send(rows);
+            }
+            catch (err) {
+                next(err);
+            }
+        });
 
         app.get(`${ROUTE_PREFIX}process-list`, async (req, res, next) => {
             try {
@@ -135,6 +186,7 @@ function setupProcesses(app) {
 
         app.get(`${ROUTE_PREFIX}task/:id`, async (req, res, next) => {
             try {
+                await markNotifAsRead(req);
                 let opts = _.defaultsDeep({ user: req.user }, req.query);
                 let rows = await ProcessService().getTask(parseInt(req.params.id), opts);
                 res.send(rows);
@@ -190,6 +242,7 @@ function setupProcesses(app) {
 
         app.get(`${ROUTE_PREFIX}process/:id`, async (req, res, next) => {
             try {
+                await markNotifAsRead(req);
                 let opts = _.defaultsDeep({ user: req.user }, req.query);
                 let rows = await ProcessService().getProcess(parseInt(req.params.id), opts);
                 res.send(rows);
