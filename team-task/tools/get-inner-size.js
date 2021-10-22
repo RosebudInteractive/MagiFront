d4cc1a9f1eb0808ae322a942f128d0dbdc4f1a5e
staloverov <staloverov@rosebud.ru>
2021-10-22 10:33:32 +0300

    #299

---
+++ team-task/tools/get-inner-size.js
@@ -0,0 +1,17 @@
+export default function getInnerSize(element) {
+    let width = 0;
+    let height = 0;
+    if (element) {
+        const rect = element.getBoundingClientRect();
+        width = rect.width;
+        height = rect.height;
+        const cs = getComputedStyle(element);
+        const paddingX = parseFloat(cs.paddingLeft) + parseFloat(cs.paddingRight);
+        const paddingY = parseFloat(cs.paddingTop) + parseFloat(cs.paddingBottom);
+        const borderX = parseFloat(cs.borderLeftWidth) + parseFloat(cs.borderRightWidth);
+        const borderY = parseFloat(cs.borderTopWidth) + parseFloat(cs.borderBottomWidth);
+        width = element.offsetWidth - paddingX - borderX;
+        height = element.offsetHeight - paddingY - borderY;
+    }
+    return { width, height };
+}
---
+++ timeline-lib/helpers/convert-data.js
@@ -0,0 +1,47 @@
+const convertEvents = (events) => (events ? events.map((item) => {
+    const dataItem = {
+        id: item.Id ? item.Id : item.id,
+        year: +item.Year,
+        name: item.Name,
+        color: item.color,
+    };
+    if (item.ShortName)
+        dataItem.shortName = item.ShortName;
+    if (item.Description)
+        dataItem.description = item.Description;
+    if (item.Day)
+        dataItem.day = +item.Day;
+    if (item.Month)
+        dataItem.month = +item.Month;
+    return dataItem;
+}) : []);
+const convertPeriods = (periods) => (periods
+    ? periods.map((item) => {
+        const dataItem = {
+            id: item.Id ? item.Id : item.id,
+            startYear: +item.LbYear,
+            endYear: +item.RbYear,
+            name: item.Name,
+            color: item.color,
+        };
+        if (item.ShortName)
+            dataItem.shortName = item.ShortName;
+        if (item.Description)
+            dataItem.description = item.Description;
+        if (item.LbDay)
+            dataItem.startDay = +item.LbDay;
+        if (item.LbMonth)
+            dataItem.startMonth = +item.LbMonth;
+        if (item.RbDay)
+            dataItem.endDay = +item.RbDay;
+        if (item.RbMonth)
+            dataItem.endMonth = +item.RbMonth;
+        return dataItem;
+    }) : []);
+export default function convertData(data) {
+    const { Events, Periods } = data;
+    return {
+        Events: convertEvents(Events),
+        Periods: convertPeriods(Periods),
+    };
+}
