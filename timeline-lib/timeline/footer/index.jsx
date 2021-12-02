b70d0684fedf7ad8cb10454d1a4dc9f419d8b8a7
staloverov <staloverov@rosebud.ru>
2021-12-02 18:47:47 +0300

    #305
    # Conflicts:
    #	timeline-lib/timeline/footer/index.jsx

--- timeline-lib/timeline/footer/index.jsx
+++ timeline-lib/timeline/footer/index.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { TouchableOpacity, View, } from 'react-native';
 import style from './style';
 import OpenButton from './open-full-screen';
 import CloseButton from './close-full-screen';
 import ZoomSlider from './zoom-slider';
 export default function Footer(props) {
-    const { fullScreenMode, zoom, onOpenPress, onClosePress, onZoomChange, onSliderStop, } = props;
+    const { fullScreenMode, zoom, onOpenPress, onClosePress, onZoomChange, onSliderStop, enableToSwitchFS, } = props;
+    const button = useMemo(() => {
+        if (!enableToSwitchFS)
+            return null;
+        return fullScreenMode
+            ? (<TouchableOpacity onPress={onClosePress}>
+          <View style={style.button}>
+            <CloseButton />
+          </View>
+        </TouchableOpacity>)
+            : (<TouchableOpacity onPress={onOpenPress}>
+          <View style={style.button}>
+            <OpenButton />
+          </View>
+        </TouchableOpacity>);
+    }, [enableToSwitchFS, fullScreenMode, onOpenPress, onClosePress]);
     return (<View style={[style.footer]}>
       <ZoomSlider value={zoom} onChange={onZoomChange} onSliderStop={onSliderStop}/>
-      {fullScreenMode
-            ? (<TouchableOpacity onPress={onClosePress}>
-            <View style={style.button}>
-              <CloseButton />
-            </View>
-          </TouchableOpacity>)
-            : (<TouchableOpacity onPress={onOpenPress}>
-            <View style={style.button}>
-              <OpenButton />
-            </View>
-          </TouchableOpacity>)}
+      {button}
     </View>);
 }
--- timeline-lib/timeline/time-axis/periods/styles.js
+++ timeline-lib/timeline/time-axis/periods/styles.js
@@ -1,24 +1,26 @@
 import { StyleSheet } from 'react-native';
+
 export default StyleSheet.create({
-    period: {
-        position: 'absolute',
-        borderRadius: 4,
-        flexDirection: 'row',
-        overflow: 'hidden',
-        height: 24,
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        cursor: 'pointer',
-        paddingHorizontal: 8,
-    },
-    title: {
-        color: 'white',
-        fontFamily: 'Fira Sans',
-        fontWeight: '400',
-        fontSize: 10,
-    },
-    dateTitle: {
-        opacity: 0.57,
-        marginRight: 8,
-    },
+  period: {
+    position: 'absolute',
+    borderRadius: 4,
+    flexDirection: 'row',
+    overflow: 'hidden',
+    height: 24,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    cursor: 'pointer',
+  },
+  title: {
+    color: 'white',
+    fontFamily: 'Fira Sans',
+    fontWeight: '400',
+    fontSize: 10,
+    marginHorizontal: 8,
+  },
+  dateTitle: {
+    opacity: 0.57,
+    marginLeft: 8,
+    marginRight: 0,
+  },
 });
